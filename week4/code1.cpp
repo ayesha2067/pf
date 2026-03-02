33508b83b048e3e4078b3bc56fd86e6b9228463a
ayesha2067 <ayeshamazhar2067@gmail.com>
2026-03-02 22:38:45 +0500

    message

---
+++ week4/code1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+main(){
+    cout<<"this is my first program in vs code"<<endl;
+    
+}
---
+++ week4/inta.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main(){ 
+    int a;
+    cout<<"enter a number:";
+    cin>>a;
+    cout<<"the number you entered is:"<<a<<endl;
+    
+}
